fbafb7bbef1b7617cc7860d04e265407dbded91a
977all30 <65038702+977all30@users.noreply.github.com>
2020-05-11 19:32:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function onClickMenu(){
 	
 	document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+var index=1;
+cycleforward(index);
+
+function indexfinder(x)
+{
+	cycleforward(index+=x);
+}
+function cycleforward(n) {
+	var picarray=document.getElementsByClassName("slides");
+	
+	if(n>picarray.length)
+{
+	index=1;
+}
+if(n<1)
+	{
+		index=picarray.length;
+	}
+	for(var i=0;i<picarray.length;i++)
+		{
+			picarray[i].style.display="none";
+		}
+	picarray[index-1].style.display="inline";
+}
